f7b4cd54da73a0be68e1f03d024f0839f557c04a
maxichrome <maxichrome@users.noreply.github.com>
2022-05-14 19:38:24 -0500

    add date processing python script

---
+++ nlp/process.py
@@ -0,0 +1,25 @@
+from sys import argv
+import recurrent
+from json import JSONEncoder as encoder
+
+json = encoder().encode
+
+try:
+	text_content = ' '.join(argv[1:])
+	parsed_result = recurrent.parse(text_content) or None
+	friendly_result = recurrent.format(parsed_result) if parsed_result else None
+
+	print(
+		json({
+			"parameterised": parsed_result,
+			"friendly": friendly_result if not friendly_result.startswith("RRULE") else None,
+		})
+	)
+	exit(0)
+except Exception as ex:
+	print(
+		json({
+			"message": ex.args[0]
+		})
+	)
+	exit(1)
